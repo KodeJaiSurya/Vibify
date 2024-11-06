8efb5635b034beaaf6c333d45e32f856bf706b19
KodeJaiSurya <59638236+KodeJaiSurya@users.noreply.github.com>
2024-11-05 20:30:23 -0500

    testing loggers working

--- pipelines/data_pipeline/dags/src/anomaly.py
+++ pipelines/data_pipeline/dags/src/anomaly.py
@@ -2,6 +2,7 @@ import tensorflow_data_validation as tfdv
 import pandas as pd
 import numpy as np
 import logging
+# Added loggers
 
 # Configure logging
 logging.basicConfig(level=logging.INFO, format='%(asctime)s - %(levelname)s - %(message)s')
