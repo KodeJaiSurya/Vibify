63999ca115bc39de13341a7f4c7baa5f2f834bab
KodeJaiSurya <59638236+KodeJaiSurya@users.noreply.github.com>
2024-11-05 23:38:13 -0500

    Update anomaly.py

--- pipelines/data_pipeline/dags/src/anomaly.py
+++ pipelines/data_pipeline/dags/src/anomaly.py
@@ -1,19 +1,16 @@
 import tensorflow_data_validation as tfdv
 import pandas as pd
-import numpy as np
 import logging
-# Added loggers
 
 # Configure logging
 logging.basicConfig(level=logging.INFO, format='%(asctime)s - %(levelname)s - %(message)s')
 logger = logging.getLogger(__name__)
 
-def getAnomalies(array: np.array):
+def getAnomalies(df: pd.DataFrame):
     logger.info("Starting anomaly detection process.")
     
     # Generate data statistics from the input DataFrame
     logger.info("Generating data statistics.")
-    df = pd.DataFrame(array)
     data_stats = tfdv.generate_statistics_from_dataframe(df)
     
     # Infer a schema from the generated statistics
@@ -28,5 +25,11 @@ def getAnomalies(array: np.array):
     if anomalies.anomaly_info:
         logger.warning("Anomalies detected in the data.")
         tfdv.display_anomalies(anomalies)
+        raise ValueError("Anomalies detected in the dataset. Workflow aborted.")
     else:
         logger.info("No anomalies detected.")
+
+
+if __name__ == "__main__":
+    getAnomalies(pd.read_csv("dags/data/raw/dataset_fer2013.csv"))
+    getAnomalies(pd.read_csv("dags/data/raw/song_dataset.csv"))
