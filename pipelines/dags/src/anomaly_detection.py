505ad02ebf374ca188b4d093643b0c38c15adb95
Kirti <kirtikshirsagar8@gmail.com>
2024-12-08 15:46:23 -0500

    data

--- pipelines/dags/src/anomaly_detection.py
+++ pipelines/dags/src/anomaly_detection.py
@@ -64,9 +64,9 @@ def process_files(data_dir: str, specific_file: str = ''):
 
 if __name__ == '__main__':
     import sys
-    # data_dir = sys.argv[1] if len(sys.argv) > 1 else os.path.abspath('./pipelines/dags/data') '/Vibify/data'
-    base_dir = os.path.abspath(os.path.join(os.path.dirname(__file__), '..', '..'))
-    data_dir = sys.argv[1] if len(sys.argv) > 1 else os.path.join(base_dir, 'Vibify', 'data')
+    data_dir = sys.argv[1] if len(sys.argv) > 1 else os.path.abspath('./pipelines/dags/data')
+    # base_dir = os.path.abspath(os.path.join(os.path.dirname(__file__), '..', '..'))
+    # data_dir = sys.argv[1] if len(sys.argv) > 1 else os.path.join(base_dir, 'Vibify', 'data')
     specific_file = sys.argv[2] if len(sys.argv) > 2 else ''
     process_files(data_dir, specific_file)
 
